b4ff0ebad2841cdc135267153d27713cdb2b267f
joyce <joycellorera2@gmail.com>
2026-08-16 07:59:58 +0800

    added vids

--- js/script.js
+++ js/script.js
@@ -156,3 +156,16 @@ if (boxes && leftBtn && rightBtn) {
         });
     });
 }
+
+//video play
+const videos = document.querySelectorAll('video');
+
+videos.forEach(video => {
+    video.addEventListener('play', () => {
+        videos.forEach(otherVideo => {
+            if (otherVideo !== video) {
+                otherVideo.pause();
+            }
+        });
+    });
+});
